fbe1c0414a11e16e39f9fd895ee92c992b0d6943
Chip Goines <chip_goines@harvard.edu>
2023-01-17 19:14:39 -0500

    added env var for itest mongo collection, added integration_test flag message for integration testing alongside production codepath, reverted spervsor celery entry to original

--- app/resources.py
+++ app/resources.py
@@ -34,7 +34,7 @@ def define_resources(app):
     transformer_endpoint = os.environ.get('TRANSFORMER_ENDPOINT')
     mongo_url = os.environ.get('MONGO_URL')
     mongo_dbname = os.environ.get('MONGO_DBNAME')
-    mongo_collection = os.environ.get('MONGO_COLLECTION')
+    mongo_collection_name = os.environ.get('MONGO_COLLECTION')
     mongo_ssl_cert = os.environ.get('MONGO_SSL_CERT')
     sleep_secs = int(os.environ.get('SLEEP_SECS', 2))
 
@@ -56,18 +56,15 @@ def define_resources(app):
         #res = client.send_task('tasks.tasks.do_task', args=[{"job_ticket_id":"123","hello":"world"}], kwargs={}, queue=incoming_queue)
         #read from 'final_queue' to see that it went through the pipeline
         job_ticket_id = str(random.randint(1, 4294967296))
-        test_message = {"job_ticket_id":job_ticket_id, "hello":"world"}
+        test_message = {"job_ticket_id":job_ticket_id, "integration_test": True}
         task_result = do_task(test_message)
         task_id = task_result.id
 
         #read from mongodb
         try:
-            mongo_url = os.environ.get('MONGO_URL')
-            mongo_dbname = os.environ.get('MONGO_DBNAME')
             mongo_client = MongoClient(mongo_url, maxPoolSize=1)
-
             mongo_db = mongo_client[mongo_dbname]
-            integration_collection = mongo_db['integration_test']
+            mongo_collection = mongo_db[mongo_collection_name]
 
             harvester_filter = {"id": "harvester-" + job_ticket_id, "name": "Harvester"}
             transformer_filter = {"id": "transformer-" + job_ticket_id, "name": "Transformer"}
@@ -77,7 +74,7 @@ def define_resources(app):
             time.sleep(sleep_secs) #wait for queue 
             for component_filter in filters:
                 query  = { "id": component_filter["id"] }
-                itest_record = integration_collection.find_one(query)
+                itest_record = mongo_collection.find_one(query)
                 if (itest_record == None):
                     result["num_failed"] += 1
                     result["tests_failed"].append(component_filter["name"])
