18c770aebb9fec236192562b6d9000ffb3e1a26b
chip goines <chip_goines@harvard.edu>
2023-02-06 16:07:52 -0500

    SS-159: checks for s3 uploads (#7)

--- app/resources.py
+++ app/resources.py
@@ -11,7 +11,7 @@ import ssl
 from pymongo import MongoClient
 #from celery import Celery
 from tasks.tasks import do_task, get_end_message
-import random
+import random, boto3
 
 
 incoming_queue = os.environ.get('FIRST_QUEUE_NAME', 'first_queue')
@@ -31,6 +31,22 @@ def define_resources(app):
     mongo_collection_name = os.environ.get('MONGO_COLLECTION')
     mongo_ssl_cert = os.environ.get('MONGO_SSL_CERT')
     sleep_secs = int(os.environ.get('SLEEP_SECS', 2))
+
+    via_access_key = os.environ.get('S3_VIA_ACCESS_KEY')
+    via_secret_key = os.environ.get('S3_VIA_SECRET_KEY')
+    via_bucket_name = os.environ.get('S3_VIA_BUCKET')
+    via_s3_endpoint = os.environ.get('S3_VIA_ENDPOINT')
+    via_s3_region = os.environ.get('S3_VIA_REGION')
+    via_test_object = os.environ.get('S3_VIA_TEST_OBJECT')
+
+    ssio_access_key = os.environ.get('S3_SSIO_ACCESS_KEY')
+    ssio_secret_key = os.environ.get('S3_SSIO_SECRET_KEY')
+    ssio_bucket_name = os.environ.get('S3_SSIO_BUCKET')
+    ssio_s3_endpoint = os.environ.get('S3_SSIO_ENDPOINT')
+    ssio_s3_region = os.environ.get('S3_SSIO_REGION')
+    ssio_test_object = os.environ.get('S3_SSIO_TEST_OBJECT')
+
+    s3_test_prefix = os.environ.get('S3_TEST_PREFIX')
 
     # Version / Heartbeat route
     @dashboard.route('/version', endpoint="version", methods=['GET'])
@@ -77,8 +93,44 @@ def define_resources(app):
         except Exception as err:
             result["num_failed"] += 1
             result["tests_failed"].append("Mongo")
-            result["Failed Mongo"] = {"status_code": 500, "text": "Failed mongo connection"}
+            result["Failed Mongo"] = {"status_code": 500, "text": str(err) }
             mongo_client.close()
+
+        #Check S3 buckets
+        try:
+            via_boto_session = boto3.Session(aws_access_key_id=via_access_key, aws_secret_access_key=via_secret_key)
+            via_s3_resource = via_boto_session.resource('s3')
+            via_s3_bucket = via_s3_resource.Bucket(via_bucket_name)
+
+            try:
+                via_s3_bucket.Object(via_test_object).last_modified
+            except Exception as err:
+                result["num_failed"] += 1
+                result["tests_failed"].append("VIA")
+                result["Failed VIA bucket"] = {"status_code": 500, "text": str(err) }
+                traceback.print_exc()
+
+            ssio_boto_session = boto3.Session(aws_access_key_id=ssio_access_key, aws_secret_access_key=ssio_secret_key)
+            ssio_s3_resource = ssio_boto_session.resource('s3')
+            ssio_s3_bucket = ssio_s3_resource.Bucket(ssio_bucket_name)
+
+            try:
+                ssio_s3_bucket.Object(ssio_test_object).last_modified
+            except Exception as err:
+                result["num_failed"] += 1
+                result["tests_failed"].append("SSIO")
+                result["Failed SSIO bucket"] = {"status_code": 500, "text": str(err) }
+                traceback.print_exc()
+
+            # delete contents of s3 test export folder to reset test
+            via_s3_bucket.objects.filter(Prefix=s3_test_prefix).delete()
+            ssio_s3_bucket.objects.filter(Prefix=s3_test_prefix).delete()
+
+        except Exception as err:
+            result["num_failed"] += 1
+            result["tests_failed"].append("S3")
+            result["Failed S3"] = {"status_code": 500, "text": str(err) }
+
 
         return json.dumps(result)
 
